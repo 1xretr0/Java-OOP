70db58a9cd9dee0a0ef5bce492dcbf557a076d7c
Retr0 <sebasmorandez@hotmail.com>
2022-02-11 13:52:48 -0600

    Ejercicio 2.2 finalizado

--- Ejercicio 2.2/Hora.java
+++ Ejercicio 2.2/Hora.java
@@ -5,7 +5,7 @@
  */
 public class Hora
 {
-    private int segundos = 0;
+    private int segundos;
     
     //  Zona de consttructores
    public Hora(int h, int m, int s) {
@@ -33,40 +33,69 @@ public class Hora
    }
    
    // Zona de las operaciones
-   // Modificadores  (setters)
+   // setters()
    public void setHoras(int h) {
+       //remover ultimo valor de horas en segundos
+       segundos -= (segundos / 3600) * 3600;
+       
+       //validar horas
        int horas = (h >= 0 && h <= 23) ?  h :  0;
        segundos += horas * 3600;
    }
    
    public void setMinutos(int m) {
+       //remover ultimo valor de minutos en segundos
+       segundos -= ((segundos % 3600) / 60) * 60;
+       
        int minutos = (m >= 0 && m <= 59) ?  m :  0;
        segundos += minutos * 60;
    }
    
    public void setSegundos(int s) {
-       segundos += (s >= 0 && s <= 59) ?  s :  0;
+       segundos -= ((segundos % 3600) % 60);
+       
+       segundos += (s >= 0 && s <= 59) ? s : 0;
    }
    
    // Selectores (getters)
    public int getSegundos() {
-       return segundos;
+       return (segundos % 3600) % 60;
    }
    
    public int getMinutos(){
-       return segundos / 60;
+       return (segundos % 3600) / 60;
    }
    
    public int getHoras(){
        return segundos / 3600;
    }
    
+   //custom methods
    public String toString() {
-       int horas = segundos / 60;
-       int minutos = 
+       int horas = segundos / 3600;
+       int minutos = (segundos % 3600) / 60;
+       int segundos_aux = (segundos % 3600) % 60;
+       
        return ((horas == 0 || horas == 12) ? 12 : horas % 12) + ":" +
                (minutos < 10 ? "0" : "") + minutos + ":" +
-               (segundos < 10 ? "0" : "") + segundos + 
-               (horas <12 ? " AM" : " PM"); 
+               (segundos_aux < 10 ? "0" : "") + segundos_aux + 
+               (horas < 12 ? " AM" : " PM"); 
+   }
+   
+   public String toMilitaryString(){
+       return (segundos / 3600 < 10 ? "0" : "") + (segundos / 3600) + 
+       (((segundos % 3600) / 60) < 10 ? "0" : "") + ((segundos % 3600) / 60) + " hrs";
+    }
+    
+    
+   public int tick(){
+       if (segundos == 86399){
+           segundos = 0;
+       }
+       else{
+           segundos++;
+       }
+       
+       return segundos;
    }
 }
