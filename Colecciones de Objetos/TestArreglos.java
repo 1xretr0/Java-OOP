d47141eecaeb7532bcfb54b416a6b5760a9609c6
sebas moran <sebasmorandez@hotmail.com>
2022-03-14 17:37:47 -0600

    updated exercise code

--- Colecciones de Objetos/TestArreglos.java
+++ Colecciones de Objetos/TestArreglos.java
@@ -1,23 +1,26 @@
-public class TestArreglos{
-    private int arreglo[];  // declaracion de un bojeto arreglo
-    private int []arreglo2 = new int[5];
-    private int[] arreglo3;
+public class TestArreglos
+{
+    private int arreglo[], arreglo1;
+    private int []arreglo2, arreglo4 = new int[10]; 
+    private int[] arreglo3, arreglo5 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; 
     private int atributo;
     
-    public TestArreglos(){
-        arreglo = new int[5];
-        arreglo3 = new int[10];
-        for (int i = 0; i < arreglo3.length; i++)
-            arreglo3[i] = i;
-            
-        atributo = 10;
-        ejemplo(atributo);
+    public TestArreglos()
+    {
+        arreglo = new int[20];
+        arreglo2 = new int[20];
+        arreglo = new int[50];
         
+        for(int i = 0; i < arreglo.length; i++ )
+            arreglo[i] = i;
+        
+        atributo = 10;
+        System.out.println(ejemplo(arreglo));
     }
     
-    public int ejemplo(int valor){
-        valor = 2;
-        return valor;
+    public int ejemplo(int valor[]){
+        valor[3] = 2;
+        return valor[3];
     }
     
     public static void main(String[] args){
