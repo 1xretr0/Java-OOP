fc15ceec032c86cc8f5c69842774b95d78d9033c
sebas moran <sebasmorandez@hotmail.com>
2022-05-10 19:01:13 -0500

    progress on puntos ecuacion project inherited from frame still painting graphic table

--- Ejercicio 5.1/PuntosEcuacion.java
+++ Ejercicio 5.1/PuntosEcuacion.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.Vector;
+import java.awt.geom.*;
 /**
  * conserva los puntos representados por un polinomio
  * en un intervalo dado. generara una lista de valores
@@ -14,17 +15,25 @@ public class PuntosEcuacion extends Frame
     private Vector <Punto> puntos;
     private double linf, lsup, inc;
     
-    /**
-     * Constructor for objects of class PuntosEcuacion
-     */
     public PuntosEcuacion(Polinomio poli, double linf, double lsup,
     double inc)
     {
+        super("PuntosEcuacion");
+        
         this.poli = poli;
         puntos = new Vector<Punto>(1);
         this.linf = linf;
         this.lsup = lsup;
         this.inc = inc;
+    }
+    
+    public boolean handleEvent(Event e){
+        if (e.id == Event.WINDOW_DESTROY){
+            setVisible(false); dispose();
+            // hide() esta deprecated
+            return true;
+        }
+        return super.handleEvent(e);
     }
 
     /**
@@ -50,6 +59,40 @@ public class PuntosEcuacion extends Frame
         return puntos.get(num);
     }
     
+    public void paint(Graphics gc1d){
+        Graphics2D gc = (Graphics2D) gc1d;
+        
+        gc.translate(250,250);
+        gc.setColor(Color.black);
+        gc.setStroke(new BasicStroke(2f));
+        // linea horizontal
+        gc.draw(new Line2D.Float(-250, 0, 250, 0));
+        gc.draw(new Line2D.Float(-250, 100, 250, 100));
+        gc.setColor(Color.gray);
+        gc.setStroke(new BasicStroke(1.0f));
+        for (int i = 10; i <= 250; i += 20){
+            gc.draw(new Line2D.Float(250, i, -250, i));
+        }
+        
+        for (int i = -10; i >= -250; i -= 20){
+            gc.draw(new Line2D.Float(250, i, -250, i));
+        }
+        
+        gc.setColor(Color.black);
+        gc.setStroke(new BasicStroke(2f));
+        // linea vertical
+        gc.draw(new Line2D.Float(0, 250, 0, -250));
+        gc.setColor(Color.gray);
+        gc.setStroke(new BasicStroke(1.0f));
+        for (int i = 10; i <= 250; i += 20){
+            gc.draw(new Line2D.Float(i, 250, i, -250));
+        }
+        
+        for (int i = -10; i >= -250; i -= 20){
+            gc.draw(new Line2D.Float(i, 250, i, -250));
+        }
+    }
+    
     public static void main(String args[]){
         Termino termino1 = new Termino(2, 0);
         Termino termino2 = new Termino(5, 1);
@@ -65,7 +108,8 @@ public class PuntosEcuacion extends Frame
         PuntosEcuacion puntos1 = new PuntosEcuacion(polinomio1, 1, 5, 1);
         puntos1.getPuntosEcuacion();
         // System.out.println(puntos1.getPunto(2));
-        Grafica grafica1 = new Grafica(puntos1, 1, 5, polinomio1);
         
+        puntos1.resize(500, 500);
+        puntos1.show();
     }
 }
